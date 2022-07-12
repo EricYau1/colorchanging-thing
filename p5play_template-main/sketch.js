4f3701ebe86959d173cf4e2f73f4eb311830ef89
EricYau1 <109161793+EricYau1@users.noreply.github.com>
2022-07-12 11:36:29 -0400

    Add files via upload

---
+++ p5play_template-main/sketch.js
@@ -0,0 +1,27 @@
+var color = "white"
+function setup() {
+  createCanvas(400,400);
+}
+
+
+
+function draw() {
+  background("yellow");
+  if (keyIsDown(RIGHT_ARROW)){
+    background("red");
+  }
+  if (keyIsDown(LEFT_ARROW)){
+    background("green");
+  }
+  if (keyIsDown(UP_ARROW)){
+    background("blue");
+  }
+  if (keyIsDown(DOWN_ARROW)){
+    background("purple");
+  }
+  drawSprites();
+}
+
+
+
+
